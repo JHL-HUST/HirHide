b614df97f0820c2df6b78ad649b4c0086ca5d47a
Li Chao <397302312@qq.com>
2019-08-14 19:03:32 +0800

    Add files via upload

---
+++ Hirhide/krogan2006_core/move_cyc.bat
@@ -0,0 +1,8 @@
+copy cyc_copy\detected\merge_layer12.gen  HiHiCode-MOD.gen
+copy cyc\detected_lc\merge_layer12.gen  HiHiCode-LC.gen
+copy cyc\detected_infomap\merge_layer12.gen  HiHiCode-Infomap.gen
+
+
+copy cyc_copy\baseline\MOD_.gen  MOD.gen
+copy cyc\baseline_lc\lc.gen  LC.gen
+copy cyc\baseline_inf\infomap.gen  Infomap.gen
