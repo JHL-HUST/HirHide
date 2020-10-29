6169d95c51425ab26772e54a6bc90bcd661611c3
Li Chao <D201880880@hust.edu.cn>
2020-10-29 18:04:28 +0800

    datasets
    datasets update

--- Hirhide/YeastNet/move_mips.bat
+++
@@ -1,7 +0,0 @@
-copy mips_copy\detected\merge_layer12.gen  HiHiCode-MOD.gen
-copy mips\detected_lc\merge_layer12.gen  HiHiCode-LC.gen
-copy mips\detected_infomap\merge_layer12.gen  HiHiCode-Infomap.gen
-     
-copy mips_copy\baseline\MOD_.gen  MOD.gen
-copy mips\baseline_lc\lc.gen  LC.gen
-copy mips\baseline_inf\infomap.gen  Infomap.gen
